4c23b9849fb2fa21ec479dcfce013666a418ff8c
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-08-20 20:56:39 -0300

    refactor: remove unnecessary module

--- src/proxyrmq/proxyrmq.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ClientProxyPdiGazin } from './client-proxy';
-
-@Module({
-  providers: [ClientProxyPdiGazin],
-  exports: [ClientProxyPdiGazin],
-})
-export class ProxyrmqModule {}
